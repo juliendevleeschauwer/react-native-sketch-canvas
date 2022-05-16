8eeb8ac6e0a946e6e512a1c0729108b9bdee93f7
JDEVLEESCHAUWER <73562484+juliendevleeschauwer@users.noreply.github.com>
2022-05-16 16:54:45 +0200

    16 05 2022

--- index.d.ts
+++ index.d.ts
@@ -61,6 +61,7 @@ export interface LocalSourceImage {
 }
 
 export interface SketchCanvasProps {
+  defaultPaths : Path[];
   style?: StyleProp<ViewStyle>
   strokeColor?: string
   strokeWidth?: number
